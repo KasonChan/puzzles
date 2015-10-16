0ac3a554bbab725044482f0468abd7f30eeff2ac
KasonChan <kasonl.chan@gmail.com>
2015-10-16 13:30:48 -0500

    Add S35; update Helpers

---
+++ src/main/scala/S99/S35.scala
@@ -0,0 +1,22 @@
+package S99
+
+import helpers.Helpers.primeFactors
+
+/**
+ * Created by kasonchan on 10/16/15.
+ */
+object S35 extends App {
+
+  //  6 Problem 35
+  //  (**) Determine the prime factors of a given positive integer. Construct a
+  //  flat list containing the prime factors in ascending order.
+  //  Example:
+  //  (prime-factors 315)
+  //  (3 3 5 7)
+  //  Example in Haskell:
+  //  primeFactors 315
+  //  [3, 3, 5, 7]
+
+  println(primeFactors(315))
+
+}
--- src/main/scala/helpers/Helpers.scala
+++ src/main/scala/helpers/Helpers.scala
@@ -5,6 +5,15 @@ package helpers
  */
 object Helpers {
 
+  def isPrime(i: Int): Boolean = {
+    if (i <= 1)
+      false
+    else if (i == 2)
+      true
+    else
+      !(2 to (i - 1)).exists(x => i % x == 0)
+  }
+
   def primeFactors(num: Long): List[Long] = {
     val exists = (2L to math.sqrt(num).toLong).find(num % _ == 0)
     exists match {
@@ -12,5 +21,15 @@ object Helpers {
       case None => List(num)
     }
   }
+
+  def primes = Stream.cons(2, Stream.from(3, 2) filter {
+    isPrime(_)
+  })
+
+  def gcd(x: Int, y: Int): Int = if (y == 0) x else gcd(y, x % y)
+
+  def coprime(x: Int, y: Int): Boolean = gcd(x, y) == 1
+
+  def totient(m: Int): Int = (1 to m).count(coprime(m, _))
 
 }
